ba40c5ba8077033e49b526fab1488c7216967178
md.munna <mk8761174@gmail.com>
2025-09-18 09:38:59 +0600

    51-5 Basic routing concept and nested routing

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <h1 className='text-4xl'>Profile Page</h1>
+        </div>
+    );
+};
+
+export default ProfilePage;
