5fe914708764f0a95830982638ede124c89c1a69
md.munna <mk8761174@gmail.com>
2025-09-19 07:31:54 +0600

    51-10 Explore built-in CSS module support. Module summary.

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import style from "./About.module.css"
 
 const AboutPage = () => {
     return (
         <div>
-            <h1 className=' text-4xl'>About Page</h1>
+            <h1 className={style.text_style}>About Page</h1>
         </div>
     );
 };
